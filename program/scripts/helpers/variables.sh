906aaa8fc65ea384422d3dec2afb49bfa83d90c0
progaproga <ni.kuchmenko@gmail.com>
2025-05-14 21:34:51 +0300

    delete лишние переменны

--- program/scripts/helpers/variables.sh
+++ program/scripts/helpers/variables.sh
@@ -1,19 +1,5 @@
 #!/bash/sh
 
-p_blue="$(tput setaf 6)"
-p_res="$(tput sgr0)"
-
-red="\033[0;31m"
-blue="\033[0;34m"
-green="\033[0;32m"
-yellow="\033[0;33m"
-i_yellow='\033[93m'
-
-
-bold="\033[1m"
-nc="\033[0m"
-alfv="abcdefghijklmnopqrstuvwxyz"
-
 variant_menu=(
         "1 - Контрольный расчет для n точек            "
         "2 - Расчёт параметра с заданной точностью     "
