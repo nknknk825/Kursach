3c0b848fa07eece111a0c47f76688b53d6fecfeb
progaproga <ni.kuchmenko@gmail.com>
2025-05-16 11:58:46 +0300

    fix

--- program/scripts/menu.sh
+++ program/scripts/menu.sh
@@ -9,7 +9,6 @@ variant_menu=(
         "1 - Контрольный расчет для n точек            "
         "2 - Расчёт параметра с заданной точностью     "
         "3 - Запись данных в файлы                     "
-        "p - Вывод пояснений к параметрам              "
         "q - Выход из программы                        "
 )
 
@@ -74,17 +73,17 @@ pg1() {
     read -a header <<< "${out_data[0]}"   # Чтение первой строки как заголовок (не используется далее)
 
     # Печать заголовка таблицы в консоль
-    printf "\n	%-10s %8s %10s %9s\n" "   №" "t" "Uvx" "Uvix"
+    printf "\n	%-4s %9s %10s %9s\n" "   №" "t" "Uvx" "Uvix"
 
     # Запись заголовка таблицы в файл
-    printf "%-4s %10s %9s %8s\n" "№" "t" "Uvx" "Uvix" > "data/tabls/table_krnt.txt"
+    printf "%-4s %4s %9s %8s\n" "№" "t" "Uvx" "Uvix" > "data/tabls/table_krnt.txt"
 
     # Печать и запись каждой строки таблицы
     for i in "${!t[@]}"; do
-        printf "        %5d %9.7f %9.1f %9.1f\n" \
+        printf "        %5d %9.8f %9.1f %9.1f\n" \
             "$((i+1))" "${t[$i]}" "${Uvx[$i]}" "${Uvix[$i]}"
 
-        printf "%5d %9.1f %9.7f %9.1f\n" \
+        printf "%5d %9.1f %9.1f %9.1f\n" \
             "$((i+1))" "${t[$i]}" "${Uvx[$i]}" "${Uvix[$i]}" >> "data/tabls/table_krnt.txt"
     done
 
@@ -148,161 +147,158 @@ out_zast(){
 }
 
 # Функция отображения основного меню
-out_menu() {
+
+clear
+inp_data=()    # Массив входных данных
+out_data=()    # Массив выходных данных
+
+out_zast # Отображение заставки out_menu # Запуск главного меню
+
+while true; do
+
+    echo -e "Меню программы:"
+    for indx in "${!variant_menu[@]}"; do
+            echo "${variant_menu[${indx}]}"
+    done
+    echo
     while true; do
 
-        echo -e "Меню программы:"
-        for indx in "${!variant_menu[@]}"; do
-                echo "${variant_menu[${indx}]}"
-        done
-        echo
-        while true; do
+        echo -n "Выберите действие 1-3 или q для выхода "
+        read -rsn1 key    # Чтение одного символа
+		printf "\n"
+    	cn_vr=2
+        case $key in
+            1|2)
 
-
-            echo -n "Выберите действие 1-3 и p или q для выхода "
-            read -rsn1 key    # Чтение одного символа
-			printf "\n"
-        	cn_vr=2
-            case $key in
-                1|2)
-                    clear
-                    echo "Ведите n точек:"
-                    echo "Диапазон n: [2;${N}]"
-                    while true; do
-                        is_number "	Ведите n: " '^[0-9]+$'    # Проверка ввода целого числа
-                        if [ "$num" -gt "1" ]; then
-                            if [ "$num" -le "$N" ]; then break
-                            else
-                                clear_line
-                                echo "	Error: Число ($num) > $N"
-                            fi
+            	info_n=(
+            		"null"
+            		"Количество точек расчёта"
+            		"Начало осчета параметро eps"
+            	)
+                clear
+                echo "Ведите n(${info_n[$key]}):"
+                echo "Диапазон n: [2;${N}]"
+                while true; do
+                    is_number "	Ведите n: " '^[0-9]+$'    # Проверка ввода целого числа
+                    if [ "$num" -gt "1" ]; then
+                        if [ "$num" -le "$N" ]; then break
                         else
                             clear_line
-                            echo "	Error: Число ($num) < 2"
+                            echo "	Error: Число ($num) > $N"
                         fi
-                    done
-                    n=$num    # Сохранение введённого значения
-                ;;&
+                    else
+                        clear_line
+                        echo "	Error: Число ($num) < 2"
+                    fi
+                done
+                n=$num    # Сохранение введённого значения
 
-                2)
-                    echo "Ведите погрешность eps:"
-                    echo "Диапазон eps: [0.001; 99.99]%"
+				if [ "$key" == "2" ];then
+                    echo "Ведите погрешность eps(допустимая погрешность):"
+                    echo "Диапазон eps: [0.001; 10]%"
                     while true; do
                         is_number "	Введите eps: " '^[0-9]*\.?[0-9]+$'    # Проверка вещественного числа
 
                         # Проверка: num > 0.0009
                         valid_min=$(echo "$num > 0.0009" | bc -l)
-                        # Проверка: num < 99.99
-                        valid_max=$(echo "$num < 99.99" | bc -l)
+                        # Проверка: num < 10
+                        valid_max=$(echo "$num < 10" | bc -l)
 
                         if [[ "$valid_min" -eq 1 && "$valid_max" -eq 1 ]]; then
                             break
                         elif [[ "$valid_max" -ne 1 ]]; then
                             clear_line
-                            echo "	Ошибка: число ($num) > 99.99"
+                            echo "	Ошибка: число ($num) > 10"
                         else
                             clear_line
                             echo "	Ошибка: число ($num) < 0.0009"
                         fi
                     done
                     eps=$num    # Сохранение значения
-                ;;&
+                fi
 
-                [1-2])
-                    clear
-                    echo "Данне успешно переданны в программу!"
-                    echo "Данные из программы успешно считанны!"
-                    pg${key} $key    # Вызов функции pg1 или pg2 в зависимости от выбора
-                ;;&
+                clear
+                echo "Данне успешно переданны в программу!"
+                echo "Данные из программы успешно считанны!"
+                pg${key} $key    # Вызов функции pg1 или pg2 в зависимости от выбора
+            ;;&
 
-                3)
-                	cn_vr=2
-                	if [ "${#t[@]}" -gt "0" ];then
-                    	    clear
-						    echo "Происходит запись в файл!"
+            3)
+            	cn_vr=2
+            	if [ "${#t[@]}" -gt "0" ];then
+                	    clear
+					    echo "Происходит запись в файл!"
 
-						    # Заполнение файлов масивами t/Uvx/Uvix
-						    ./bin/prg 1 ${#t[@]} 100
+					    var_file=(                  # Массив с путями к выходным файлам
+					        "./data/massiv_t.txt"
+					        "./data/massiv_Uvx.txt"
+					        "./data/massiv_Uvix.txt"
+					    )
 
-						    clear
-						    echo "Данные успешно записанны в файл!"
-						    echo "Происходит генерация графиков пожалуйста подождите!"
+					    {
+					        for i in "${!t[@]}"; do
+					            echo "${t[$i]}"
+					        done
+					    } > "${var_file[0]}" & # фоновая запись в massiv_t.txt
 
-						    # Запуск Maxima-скрипта для построения графиков
-						    maxima -b scripts/Wxmax_scr/make_graphs.mac > /dev/null 2>&1
+					    {
+					        for i in "${!Uvx[@]}"; do
+					            echo "${Uvx[$i]}"
+					        done
+					    } > "${var_file[1]}" & # фоновая запись в massiv_Uvx.txt
 
-						    clear
-						    echo "Графики успешно нарисованы!"
-						    echo -ne "Вывести графики ? (y/n)"
-						    read -rsn1 nn
-						    if [ "$nn" == "y" ]; then
-						        echo -e "\nЗакройте окно с графиками для продолжения!"
-						        open data/graphs/graph_Uvx.png > /dev/null 2>&1    # Открытие изображения через open
-						        open data/graphs/graph_Uvix.png > /dev/null 2>&1    # Открытие изображения через open
-						    fi
-						    cn_vr=3
-                    else
-	                    clear_line
-	                    echo "Erorr: массивы t/Uvx/Uvix пусты!"
-                    fi
-                ;;&
+					    {
+					        for i in "${!Uvix[@]}"; do
+					            echo "${Uvix[$i]}"
+					        done
+					    } > "${var_file[2]}" & # фоновая запись в massiv_Uvix.txt
 
-                p)
-				    out_file_name=(
-				        "./config/explantion_krnt.txt"
-				        "./config/explantion_rpzt.txt"
-				    )
-				    clear
-				    # Вывод пояснений к водимым параметрам программы
-				    for file_name in "${out_file_name[@]}";do
-				        while IFS= read -r line;do
-				            echo "$line"
-				        done < "$file_name"
-				        if [ "$file_name" != "${out_file_name[-1]}" ];then
-				            echo -ne "\nНажмите enter чтоб перелестнуть страницу!"
-				        else
-				            echo -ne "\nНажмите enter чтоб закончить просмотр!"
-				        fi
-				        read
-				        clear
-				    done
+					    clear
+					    echo "Данные успешно записанны в файл!"
+					    echo "Происходит генерация графиков пожалуйста подождите!"
 
-	            ;;&
+					    # Запуск Maxima-скрипта для построения графиков
+					    maxima -b scripts/Wxmax_scr/make_graphs.mac > /dev/null 2>&1
 
-                [1-$cn_vr]|p)
-                    clear
-                    out_zast    # Повторный вывод заставки
-                    break
-                ;;
-
-                q)
-                    return    # Завершение работы
-                ;;
-
-                3)
-                ;;
-
-                *)
+					    clear
+					    echo "Графики успешно нарисованы!"
+					    echo -ne "Вывести графики ? (y/n)"
+					    read -rsn1 nn
+					    if [ "$nn" == "y" ]; then
+					        echo -e "\nЗакройте окно с графиками для продолжения!"
+					        open data/graphs/graph_Uvx.png > /dev/null 2>&1    # Открытие изображения через open
+					        open data/graphs/graph_Uvix.png > /dev/null 2>&1    # Открытие изображения через open
+					    fi
+					    cn_vr=3
+                else
                     clear_line
-                    echo "Erorr: Не верное значение ($key) не входит в промежуток [1;$cn_vr] и p!"
-                ;;
+                    echo "Erorr: массивы t/Uvx/Uvix пусты!"
+                fi
+            ;;&
 
-            esac
-        done
+
+            [1-$cn_vr])
+                clear
+                out_zast    # Повторный вывод заставки
+                break
+            ;;
+
+            q)
+                break 2    # Завершение работы
+            ;;
+
+            3)
+            ;;
+
+            *)
+                clear_line
+                echo "Erorr: Не верное значение ($key) не входит в промежуток [1;$cn_vr]!"
+            ;;
+
+        esac
     done
-}
+done
 
-# Функция запуска программы
-start() {
-    clear
-    inp_data=()    # Массив входных данных
-    out_data=()    # Массив выходных данных
-
-    out_zast    # Отображение заставки
-    out_menu    # Запуск главного меню
-}
-
-start    # Старт программы
 clear
 exit    # Завершение
 
